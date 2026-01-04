858283af0e3e1098019faa137481332bea349d50
anoop raju <anoopraju31@gmail.com>
2026-01-04 16:38:25 +0530

    CHORE: updated

--- src/app/room/[roomId]/page.tsx
+++ src/app/room/[roomId]/page.tsx
@@ -69,6 +69,26 @@ const RoomPage: FC = () => {
           DESTROY NOW
         </button>
       </header>
+
+      <div className="scrollbar-thin flex-1 space-y-4 overflow-y-auto p-4"></div>
+
+      <div className="border-t border-zinc-800 bg-zinc-900/30 p-4">
+        <div className="flex gap-4">
+          <div className="group relative flex-1">
+            <span className="absolute top-1/2 left-4 -translate-y-1/2 animate-pulse text-green-500"> {'>'} </span>
+            <input
+              type="text"
+              autoFocus
+              className="w-full border border-zinc-800 bg-black py-3 pr-4 pl-8 text-sm text-zinc-100 transition-colors duration-300 focus:border-zinc-700 focus:outline-none"
+            />
+          </div>
+
+          <button className="cursor-pointer bg-zinc-800 px-6 text-sm font-bold text-zinc-400 transition-all hover:text-zinc-200 disabled:cursor-not-allowed disabled:opacity-50">
+            {' '}
+            SEND{' '}
+          </button>
+        </div>
+      </div>
     </main>
   );
 };
